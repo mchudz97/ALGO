878e5dae0308ae505c461b4e1bc5a7ebe029624d
Maciej Chudzinski <mchudz97@gmail.com>
2019-12-04 23:33:09 +0100

    twojstarypijany

---
+++ Rate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ALS_RECOMMENDATION_ALGORITHM
+{
+    class Rate
+    {
+        private double value;
+        private int product;
+        private int user;
+
+        public Rate(double value, int product, int user)
+        {
+            this.value = value;
+            this.product = product;
+            this.user = user;
+
+
+        }
+        public override string ToString()
+        {
+            return "Rate: " + this.value + " Product: " + this.product+ " User: "+this.user;
+        }
+    }
+}
