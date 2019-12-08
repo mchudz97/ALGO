9db6b7e23a428046ffd38b02935894a23a554d51
Scuzz <darekburczynski@protonmail.com>
2019-12-08 19:58:04 +0100

    Changed randomize format

--- Randomizer.cs
+++ Randomizer.cs
@@ -9,7 +9,7 @@ namespace ALS_RECOMMENDATION_ALGORITHM
             double[,] matrix = new double[n,m];
             for(int i = 0 ; i < n; i++) {
                 for(int j = 0; j < m; j++) {
-                    matrix[i,j] = random.NextDouble();
+                    matrix[j,i] = random.NextDouble();
                 }
             }
             return matrix;
