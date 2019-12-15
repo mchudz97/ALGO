6dd73b9cc87d91e75eb6e2a6680566381b81acf2
Paweł <pawelekpols@gmail.com>
2019-12-15 21:35:34 +0100

    tests

--- Program.cs
+++ Program.cs
@@ -7,24 +7,30 @@ namespace ALS_RECOMMENDATION_ALGORITHM
     {
         static void Main(string[] args)
         {
-            Parser p = new Parser("amazon-meta.txt","Book", 100, 5,10);
-            foreach (KeyValuePair<String, int> kvp in p.ProductDict)
-            {
-                //textBox3.Text += ("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-            }
-            foreach (KeyValuePair<String, int> kvp in p.UserDict)
-            {
-                //textBox3.Text += ("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-            }
-            foreach (Rate r in p.RateSet)
-            {
-                Console.WriteLine(r);
-            }
 
-            // MatrixOperations mo = new MatrixOperations(p, 10);
-            // mo.ALS(0.0001);
+            // foreach (KeyValuePair<String, int> kvp in p.ProductDict)
+            // {
+            //     //textBox3.Text += ("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            //     Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            // }
+            // foreach (KeyValuePair<String, int> kvp in p.UserDict)
+            // {
+            //     //textBox3.Text += ("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            //     Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            // }
+            // foreach (Rate r in p.RateSet)
+            // {
+            //     Console.WriteLine(r);
+            // }
+
+
+
+            Parser p = new Parser("amazon-meta.txt","Book", 10, 5, 500);
+            MatrixOperations mo = new MatrixOperations(p, 5);
+            mo.test(0.0001,0.01,10);
+                
+                
+            
         }
     }
 }
